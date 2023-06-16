6725c8594a9b18b91a7125bd676be606d7e618f3
Robin Du <vpn-robin.du@vocus.com.au>
2023-06-16 16:24:10 +1000

    minor fix

--- afl_tipster/settings.py
+++ afl_tipster/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://storage.googleapis.com/afl_statics/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
